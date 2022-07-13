76627e99a81d0b4895c8574e5999fbd4796a8ef8
SubahNawarMahi <sheikhmahi1999@gmail.com>
2022-07-13 22:00:22 +0600

    Subah added ui testing for main activity, measure activity

--- CardiacRecorder/app/src/main/java/com/example/cardiacrecorder/HistoryActivity.java
+++ CardiacRecorder/app/src/main/java/com/example/cardiacrecorder/HistoryActivity.java
@@ -29,44 +29,12 @@ public class HistoryActivity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
       View  view = inflater.inflate(R.layout.fragment_history_activity, container, false);
 
-//        editButton = view.findViewById(R.id.editButton);
-    //    ListView recordList = view.findViewById(R.id.recordList);
 
-//        Record r1 = new Record("100", "70", "65", "28-JUN-2022", "10:30 AM", "Sick..", 1);
-//        Record r2 = r1;
-//        Record r3 = r1;
-//        Record r4 = r1;
-//        Record r5 = r1;
-//        Record r6 = r1;
-//        Record r7 = r1;
-//        Record r8 = r1;
-//        Record r9 = r1;
-//        Record r10 = r1;
-//
-//        ArrayList<Record> recordArrayList = new ArrayList<>();
-//
-//        recordArrayList.add(r1);
-//        recordArrayList.add(r2);
-//        recordArrayList.add(r3);
-//        recordArrayList.add(r4);
-//        recordArrayList.add(r5);
-//        recordArrayList.add(r6);
-//        recordArrayList.add(r7);
-//        recordArrayList.add(r8);
-//        recordArrayList.add(r9);
-//        recordArrayList.add(r10);
-//
         recordList = view.findViewById(R.id.recordList);
-//        // sample data
-//        List<String> list = new ArrayList<>();
-//        for(int i=0;i<100;i++)
-//            list.add("Item "+i);
-//
-//        CustomAdapter listAdapter = new CustomAdapter(list,getContext());
-        //       recordList.setAdapter(listAdapter);
+
         myDatabaseHelper = new MyDatabaseHelper(getActivity());
         sqLiteDatabase = myDatabaseHelper.getWritableDatabase();
 
--- CardiacRecorder/app/src/main/java/com/example/cardiacrecorder/MainActivity.java
+++ CardiacRecorder/app/src/main/java/com/example/cardiacrecorder/MainActivity.java
@@ -10,9 +10,7 @@ import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private Button measureButton, historyButton, addButton;
-//    private ImageButton editButton, deleteButton;
-//    private View measureUnderline, historyUnderline;
+
     TabLayout tabLayout;
     ViewPager viewPager;
 
@@ -24,60 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-//        addButton = findViewById(R.id.addButton);
-//        measureButton = findViewById(R.id.measureButton);
-//        historyButton = findViewById(R.id.historyButton);
-//
-//        measureUnderline = findViewById(R.id.measureUnderline);
-//        historyUnderline = findViewById(R.id.historyUnderline);
-//
-//        replaceFragment(new MeasureActivity());
-//        measureUnderline.setVisibility(View.VISIBLE);
-//        historyUnderline.setVisibility(View.INVISIBLE);
-//
-//        measureButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                measureUnderline.setVisibility(View.VISIBLE);
-//                historyUnderline.setVisibility(View.INVISIBLE);
-//                addButton.setVisibility(View.VISIBLE);
-//
-//                replaceFragment(new MeasureActivity());
-//
-//            }
-//        });
-//
-//        historyButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                measureUnderline.setVisibility(View.INVISIBLE);
-//                historyUnderline.setVisibility(View.VISIBLE);
-//                addButton.setVisibility(View.INVISIBLE);
-//
-//                replaceFragment(new HistoryActivity());
-//
-//            }
-//        });
-//
-//        addButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, CreateRecord.class);
-//                intent.putExtra("fromWhichActivity", "mainactivity");
-//                startActivity(intent);
-//            }
-//        });
-//
-//    }
-//
-//    private void replaceFragment(Fragment historyActivity) {
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//        fragmentTransaction.replace(R.id.frameLayout, historyActivity);
-//        fragmentTransaction.commit();
-//    }
+
+
         tabLayout = findViewById(R.id.tabLayout);
         viewPager = findViewById(R.id.viewPagerId);
 
